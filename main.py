32ad995826c86aeb539dfd39e8279a861771e1ce
sam1e-dev <64969067+sam1e-dev@users.noreply.github.com>
2021-11-19 07:40:58 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+import undetected_chromedriver.v2 as uc
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import os
+import time
+browser = uc.Chrome()
+browser.delete_all_cookies()
+os.system('cls')
+browser.get('https://popcat.click')
+input('Press Enter to start (your CPU usage may spike)')
+os.system('cls')
+print('Press Ctrl+C to stop clicking!')
+while True:
+    browser.find_element(By.XPATH, '//*[@id="app"]/div').click()
